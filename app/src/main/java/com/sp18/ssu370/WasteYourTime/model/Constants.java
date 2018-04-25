67914cc1924cb203bd0cc41dabfa200fb9edbc0c
Andy Dang <andydang456@yahoo.com>
2018-04-25 00:02:30 -0700

    I've spent sooo much money today
    Upload works.

---
+++ app/src/main/java/com/sp18/ssu370/WasteYourTime/model/Constants.java
@@ -0,0 +1,10 @@
+package com.sp18.ssu370.WasteYourTime.model;
+
+public interface Constants {
+
+        public static final int PICK_IMAGE_REQUEST = 1;
+        public static final int READ_EXTERNAL = 1001;
+        public static final int WRITE_EXTERNAL = 1002;
+        public static final int READ_WRITE_EXTERNAL = 1003;
+
+}
---
+++ app/src/main/java/com/sp18/ssu370/WasteYourTime/network/ImgurService.java
@@ -0,0 +1,31 @@
+package com.sp18.ssu370.WasteYourTime.network;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
+
+public interface ImgurService {
+    @Multipart
+    @Headers({
+            "Authorization: Client-ID 9fca26f6c035661"
+    })
+    @POST("image")
+    Call<ImageResponse> postImage(
+            @Query("title") String title,
+            @Query("description") String description,
+            @Query("album") String albumId,
+            @Query("account_url") String username,
+            @Part MultipartBody.Part file);
+
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.imgur.com/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
